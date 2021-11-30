d7500a9c80ff0cebb5a4cd402b772f96e4ab6ec0
Leandra Lopes <leandralopes477@gmail.com>
2021-11-29 21:06:03 -0300

    versão 0.0.4

---
+++ comentarios.php
@@ -0,0 +1,33 @@
+<?php
+# comnetário de uma linha
+
+// comentário de uma linha
+
+/*
+comentário de várias linhas
+pode ter várias orientações
+*/
+
+echo '<pre>
+O comentário dentrodo php pode ser feito com:
+
+# hashtag, 
+
+// barras duplas // // comentário de uma linha
+
+
+/*  */ 
+/*
+comentário de várias linhas
+pode ter várias orientações
+*/
+
+</pre>';
+
+/*
+ *variável que recebe o valor 18 
+ *
+ */
+$idade = 18;
+
+echo $idade;
